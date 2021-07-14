9621c341864857fb1e40e00a0f6d7e6d304c63d1
rpereira91 <boveah@gmail.com>
2021-07-14 12:18:31 -0400

    fixed home intro

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,11 +6,22 @@ const Home = () => {
     return (
         <Container className="pageContainer" fluid>
             <Row>
-                <span className="title">F1 info</span>
+                <span className="title">Home</span>
             </Row>
             <Row>
                 <Col sm="12" lg="6">
-                    <p>Click the header in each section to change the year</p>
+                    <p>
+                        Welcome to my Formula One info page.
+                        <br/>
+                        The Schedule page has current and historic Grand Prix calendars, each Grand Prix has a link to the wiki and a way to view the race standings.  
+                        <br/>
+                        Check out the Driver page to view current and historic driver standings, more driver information will be available in the future.  
+                        <br/>
+                        The Last Race page will have race information including qualifying and the results. The URL can be updated to view any round from any season.  
+                        <br/>
+                        The Constructor page is still under construction.  
+                    </p>
+                    <p>Click the header in each section to change the year and view historic Formula One data. </p>
                 </Col>
                 <Col sm="12" lg="6">
                     <p>
